eeb6d0ddca4db6d9109019ed21cec5e99d6f07e8
BX <bx@ubuntu.(none)>
2013-06-10 12:13:54 -0400

    Feed System Front End Reversion 1

---
+++ APP.js
@@ -0,0 +1,74 @@
+//Create Server
+var http = require("http");
+var url = require('url');
+var fs = require('fs');
+
+//Initialize Redis
+var _redis = require("redis");
+var redis = _redis.createClient();
+ 
+ //Create Server
+var server = http.createServer(function(request, response){
+    console.log('Connection');
+    var path = url.parse(request.url).pathname;
+    //console.log(path);
+
+    switch(path){
+        case '/':
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write('hello world');
+            response.end();
+            break;
+        case '/socket.html':
+            fs.readFile(__dirname+path, "utf-8",function(error, data){
+                if (error){
+                    response.writeHead(404);
+                    response.write("opps this doesn't exist - 404");
+                    response.end();
+                }
+                else{
+                    response.writeHead(200, {"Content-Type": "text/html"});
+                    response.write(data, "utf8");
+                    response.end();
+                }
+            });
+            break;
+        default:
+            response.writeHead(404);
+            response.write("opps this doesn't exist - 404");
+            response.end();
+            break;
+    }
+    //response.end();
+});
+ 
+server.listen(8001); 
+
+server.listen(8001); 
+
+var data;
+
+//Read in data from Redis
+redis.get('name', function(error, result) {
+    if (error) console.log('Error: '+ error);
+    else{
+      data=result;
+      console.log('Name: ' + data);
+    }
+});
+
+var io = require('socket.io').listen(server);
+
+io.set('log level', 1);
+ 
+io.sockets.on('connection', function(socket){
+            //Read in data from redis
+	//data='hiboy';
+
+            //Broad cast the message
+  	socket.emit('msg',{'msg':data});
+     socket.emit('msg',{'msg':data});
+     socket.emit('msg',{'msg':data});
+           //socket.broadcast.emit('msg',{'msg':data});
+});
+
